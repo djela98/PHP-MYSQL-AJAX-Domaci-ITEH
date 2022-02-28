b682574116a796424e52ed194faec6a31dd0a451
djela98 <vucendicandjela98@gmail.com>
2022-02-28 18:47:48 +0100

    Pocetna strana, meni za izbor rada sa studentima

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include 'header.php'
+?>
+
+<body>
+
+    <div class="index">
+
+        <div class="index-meni">
+            <div class="btn">
+                <button type="button" class="btn btn-success">Studenti CRUD</button>
+            </div>
+            <div class="btn">
+                <button type="button" class="btn btn-success">Pretraga i Sortiranje</button>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
